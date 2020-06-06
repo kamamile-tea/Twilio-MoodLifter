655441d9ec027672e66fa5ff2c9ed304fc674536
Kamille Tipan <31214515+kamamile-tea@users.noreply.github.com>
2020-06-06 01:11:40 -0400

    Update MoodLifter.py

--- MoodLifter.py
+++ MoodLifter.py
@@ -1,14 +1,5 @@
 # from twilio.rest import Client
 
-# client = Client(
-#   'AC2e09228d2523437f440652da9f17dec5', '8256fba14769bd1cbc3224769abb0279')
-# #9173535383
-# #giving error when call probaly due to url
-# call = client.calls.create(
-#   to='+13479937535',
-#   from_='+16812424500',
-#   url='http://demo.twilio.com/docs/voice.xml')
-
 # print(call.sid)
 
 
